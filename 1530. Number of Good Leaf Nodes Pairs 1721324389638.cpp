4c8d1e417fb6b3547bc045d3b21be1be9b74ee30
Jatin Parmar <jatinparmar980@gmail.com>
2024-07-18 23:09:50 +0530

    Time: 53 ms (41.27%), Space: 36 MB (30.32%)

---
+++ 1530. Number of Good Leaf Nodes Pairs 1721324389638.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int result=0;
+    int countPairs(TreeNode* root, int distance) {
+        dfs(root,distance);
+        return result;
+    }
+    vector<int>dfs(TreeNode*root,int d){
+        if(!root)return {};
+        if(!root->left && !root->right)return {1};
+
+        auto left=dfs(root->left,d);
+        auto right=dfs(root->right,d);
+
+        for(auto x:left)
+        for(auto y:right)if(x+y<=d)result++;
+
+        vector<int>v;
+        for(auto x:left)v.push_back(x+1);
+        for(auto x:right)v.push_back(x+1);
+        return v;
+    }
+};
