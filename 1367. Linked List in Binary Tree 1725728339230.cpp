d394282543bb96ffea422cdecf402fdd6d9bb642
Jatin Parmar <jatinparmar980@gmail.com>
2024-09-07 22:29:02 +0530

    Time: 20 ms (84.44%), Space: 30.9 MB (36.50%)

---
+++ 1367. Linked List in Binary Tree 1725728339230.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool dfs(TreeNode* root, ListNode* head)
+    {
+        if (!head)
+        {
+            return true;
+        }
+        if (!root)
+        {
+            return false;
+        }
+        return (root->val == head->val) && (dfs(root->left, head->next) || dfs(root->right, head->next));
+    }
+    bool isSubPath(ListNode* head, TreeNode* root) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        if (!head)
+        {
+            return true;
+        }
+        if (!root)
+        {
+            return false;
+        }
+        return dfs(root, head) || isSubPath(head, root->left) || isSubPath(head, root->right);
+    }
+};
