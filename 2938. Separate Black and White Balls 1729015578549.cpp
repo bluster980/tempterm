0939eb1f7eeeb430288fe152947484aca9ed7a41
Jatin Parmar <jatinparmar980@gmail.com>
2024-10-15 23:36:19 +0530

    Time: 36 ms (32.00%), Space: 15.6 MB (76.42%)

---
+++ 2938. Separate Black and White Balls 1729015578549.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+      long long ans=0;
+      int black=0;
+      for(int i=0;i<s.size();i++){
+        if(s[i]=='0'){
+            ans+=black;
+        }
+        else{
+            black++;
+        }
+      }  
+      return ans;
+    }
+};
