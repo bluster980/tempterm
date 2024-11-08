def21826c3f0435e0801fbd68646556158f01e59
Jatin Parmar <jatinparmar980@gmail.com>
2024-11-08 23:31:38 +0530

    Time: 1 ms (74.29%), Space: 95.5 MB (53.70%)

---
+++ 1829. Maximum XOR for Each Query 1731088897367.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
+        int n = nums.size();
+        vector<int>ans(n); 
+
+        int xor1 = 0;
+
+        int k = pow(2,maximumBit)-1;
+
+        for(auto i : nums)
+        {
+            xor1^=i;
+        }
+
+        int a = 0;
+
+        for(int i=0;i<n;i++)
+        {
+             ans[i] = xor1 ^ k;
+             xor1 ^= nums[n - 1 - i];
+
+        }
+
+
+        return ans;
+    }
+};
